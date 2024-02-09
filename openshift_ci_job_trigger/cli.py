c3028f82d3744c52e87cc272b965563d5a2b7e06
rnetser <rnetser@redhat.com>
2024-02-09 16:25:55 +0200

    Add files

--- openshift_ci_job_trigger/cli.py
+++ openshift_ci_job_trigger/cli.py
@@ -5,15 +5,21 @@ import requests
 import yaml
 from simple_logger.logger import get_logger
 from timeout_sampler import TimeoutSampler
+from pyaml_env import parse_config
 
 
 LOGGER = get_logger(name=os.path.split(__file__)[-1])
+GANGWAY_API_URL = "https://gangway-ci.apps.ci.l2s4.p1.openshiftapps.com/v1/executions/"
+
+
+def authorization_header(openshift_ci_token):
+    return {"Authorization": f"Bearer {openshift_ci_token}"}
 
 
 def get_prow_job_status(openshift_ci_token, triggering_job_id):
     response = requests.get(
-        url=f"https://gangway-ci.apps.ci.l2s4.p1.openshiftapps.com/v1/executions/{triggering_job_id}",
-        headers={"Authorization": f"Bearer {openshift_ci_token}"},
+        url=f"{GANGWAY_API_URL}{triggering_job_id}",
+        headers=authorization_header(openshift_ci_token=openshift_ci_token),
     )
 
     response_text = yaml.safe_load(response.text)
@@ -42,26 +48,28 @@ def triger_openshift_ci_job(
     openshift_ci_job_name,
 ):
     response = requests.post(
-        url=f"https://gangway-ci.apps.ci.l2s4.p1.openshiftapps.com/v1/executions/{openshift_ci_job_name}",
-        headers={"Authorization": f"Bearer {openshift_ci_token}"},
+        url=f"{GANGWAY_API_URL}{openshift_ci_job_name}",
+        headers=authorization_header(openshift_ci_token=openshift_ci_token),
+        json={"job_execution_type": "1"},
     )
 
-    response_text = yaml.safe_load(response.text)
-    if response.ok:
-        return response_text["job_status"]
+    if not response.ok:
+        LOGGER.error(f"Failed to get job status: {response.headers["grpc-message"]}")
+        raise click.Abort()
 
-    LOGGER.error(f"Failed to get job status: {response_text}")
-    raise click.Abort()
+    LOGGER.success(f"Successfully triggered job {openshift_ci_job_name}")
 
 
 @click.command("job-trigger`")
 @click.option(
+    "-t",
     "--openshift-ci-token",
     help="Openshift ci token, needed to trigger jobs.",
     default=os.environ.get("OPENSHIFT_CI_TOKEN"),
 )
-@click.option("--openshift-ci-job-name", help="Openshift ci job name", show_default=True, type=click.STRING)
+@click.option("-n", "--openshift-ci-job-name", help="Openshift ci job name", show_default=True, type=click.STRING)
 @click.option(
+    "-id",
     "--prow-triggering-job-id",
     help="Prow ID (prowjobid) of the job to be re-triggered. If empty, the job will be triggered immediately.",
     show_default=True,
@@ -83,6 +91,12 @@ def triger_openshift_ci_job(
     show_default=True,
 )
 def main(**kwargs):
+    user_kwargs = kwargs
+    if job_yaml_config_file := user_kwargs.get("job_yaml_config_file"):
+        # Update CLI user input from YAML file if exists
+        # Since CLI user input has some defaults, YAML file will override them
+        user_kwargs.update(parse_config(path=job_yaml_config_file, default_value=""))
+
     openshift_ci_token = kwargs.get("openshift_ci_token")
     openshift_ci_job_name = kwargs.get("openshift_ci_job_name")
     triggering_job_id = kwargs.get("prow_triggering_job_id")
