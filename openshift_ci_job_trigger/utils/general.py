898991ff5151fe945ecab29f9bedfd448f80ff44
Meni Yakove <myakove@gmail.com>
2024-02-19 11:01:11 +0200

    send_slack_message: do not raise on error

--- openshift_ci_job_trigger/utils/general.py
+++ openshift_ci_job_trigger/utils/general.py
@@ -8,6 +8,6 @@ def send_slack_message(message, webhook_url, log_prefix, app_logger):
     app_logger.info(f"{log_prefix} Sending message to slack: {message}")
     response = requests.post(webhook_url, data=json.dumps(slack_data), headers={"Content-Type": "application/json"})
     if response.status_code != 200:
-        raise ValueError(
-            f"Request to slack returned an error {response.status_code} with the following message: " f"{response.text}"
+        app_logger.error(
+            f"Request to slack returned an error {response.status_code} with the following message: {response.text}"
         )
